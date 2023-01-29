8707012c7499c943186b782324270059117a4398
Darren Delimond <darrendelimond@gmail.com>
2023-01-29 08:53:51 -0500

    =completed slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,53 +2,66 @@ import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { slider1, slider2, slider3 } from '../assets/index'
 import { useState, useEffect } from 'react';
+import sliderItems from '../data';
 
 
-const { activeSlide, setActiveSlide } = useState('0')
 
 
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0)
+    const handleClick = (direction) => {
+        direction === 'left' ? setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2) : setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+    }
+
     return (
-        <div className=" container w-screen max-w-full h-screen flex overflow-hidden relative">
-            <div onClick={() => handleClick('left')} className="flex justify-center absolute  opacity-80 left-[1rem] z-10 cursor-pointer bottom-0 top-0 m-auto items-center h-12 w-12 bg-slate-50 rounded-3xl">
-                <ArrowBackIosIcon />
+        <div className=" container w-screen max-w-full h-screen flex  relative">
+            <div className="flex justify-center absolute  opacity-80 left-[1rem] z-10 cursor-pointer bottom-60 top-0 m-auto items-center h-12 w-12 bg-slate-50 rounded-3xl">
+                <ArrowBackIosIcon onClick={() => handleClick('left')} />
             </div>
-            <div className='wrapper transition-transform m-0    translate-y-[0] flex h-full'>
-                <div className='slide flex w-screen  bg-red-200  items-center  h-screen'>
-                    <div className=' ImgContainer grow  flex justify-center h-full '>
-                        <img className='h-[90%] ml-5' src={slider1} />
-                    </div>
-                    <div className=' InfoContainer grow bg-green-500  p-12 flex flex-col'>
-                        <h1 className='text-[70px] font-bold'>SUMMER SALE</h1>
-                        <p className='font-5 my-12 max-w-lg  tracking-[.3rem]'>DON'T COMPROMISE ON STYLE! GET 30% OFF ALL NEW ARRIVALS.</p>
-                        <button className='cursor-pointer p-3 text-xl bg-transparent border-2 w-[150px] border-black  '>SHOP NOW</button>
-                    </div>
-                </div>
-                <div className='slide flex w-screen items-center  h-screen'>
-                    <div className=' ImgContainer  flex justify-center h-full '>
-                        <img className='  h-[90%] ml-5' src={slider2} />
-                    </div>
+            <div className={`wrapper transition ease-in-out delay-500 m-0    translate-x-[${slideIndex * -100}vw] flex h-full`}>
+                {sliderItems.map((slide) => {
+                    return (
+                        <div key={slide.id} className={`slide flex w-screen ${slide.bg}  items-center  h-[60vh]`}>
+                            <div className=' ImgContainer grow  flex justify-center h-full'>
+                                <img className='h-full ml-5' src={slide.img} />
+                            </div>
+                            <div className=' InfoContainer   p-12 flex flex-col'>
+                                <h1 className='text-[70px] font-bold'>{slide.title}</h1>
+                                <p className='font-5 my-12 max-w-lg  tracking-[.3rem]'>{slide.desc}</p>
+                                <button className='cursor-pointer p-3 text-xl bg-transparent border-2 w-[150px] border-black  '>SHOP NOW</button>
+                            </div>
+                        </div>
+                    )
+
+                })
+                }
+
+                {/* <div className='slide flex w-screen border-solid border-4 border-black items-center  h-[70vh]'>
+
                     <div className=' InfoContainer p-12 flex flex-col'>
                         <h1 className='text-[70px] font-bold'>CLEARANCE SALE</h1>
                         <p className='font-5 my-12 tracking-[.3rem]'>DON'T COMPROMISE ON STYLE! GET 30% OFF ALL NEW ARRIVALS.</p>
                         <button className='cursor-pointer p-3 text-xl bg-transparent border-2 w-[150px] border-black  '>SHOP NOW</button>
                     </div>
-                </div>
-                <div className='slide flex w-screen bg-[#928DB5]  items-center  h-screen'>
                     <div className=' ImgContainer  flex justify-center h-full '>
-                        <img className='h-[90%]  ml-5' src={slider3} />
+                        <img className='  h-full ml-5' src={slider2} />
+                    </div>
+                </div>
+                <div className='slide flex w-screen bg-[#928DB5]  items-center  h-[70vh]'>
+                    <div className=' ImgContainer  flex justify-center h-full '>
+                        <img className='h-full  ml-5' src={slider3} />
                     </div>
                     <div className=' InfoContainer  p-12 flex flex-col'>
                         <h1 className='text-[70px] font-bold'>POPULAR SALE</h1>
                         <p className='font-5 my-12 tracking-[.3rem]'>DON'T COMPROMISE ON STYLE! GET 30% OFF ALL NEW ARRIVALS.</p>
                         <button className='cursor-pointer p-3 text-xl bg-transparent border-2 w-[150px] border-black  '>SHOP NOW</button>
                     </div>
-                </div>
+                </div> */}
             </div>
-            <div onClick={() => handleClick('right')} className="flex justify-center absolute  opacity-80 cursor-pointer z-10 bottom-0 top-0 right-[1rem] m-auto items-center h-12 w-12 bg-slate-50 rounded-3xl">
-                <ArrowForwardIosIcon />
+            <div className="flex justify-center absolute  opacity-80 cursor-pointer z-10 bottom-60 top-0 right-[1rem] m-auto items-center h-12 w-12 bg-slate-50 rounded-3xl">
+                <ArrowForwardIosIcon onClick={() => handleClick('right')} />
             </div>
-        </div>
+        </div >
     )
 }
 
---
+++ src/data.js
@@ -0,0 +1,36 @@
+import { slider1, slider2, slider3 } from "./assets/index"
+
+const sliderItems = [
+    {
+        id: 1,
+        img: slider1,
+        title: 'SUMMER SALE',
+        desc: `DON'T COMPROMISE ON STYLE! GET 30% OFF ALL NEW ARRIVALS.`,
+        bg: 'bg-red-200'
+    },
+    {
+        id: 2,
+        img: slider2,
+        title: 'CLEARANCE SALE',
+        desc: `DON'T COMPROMISE ON STYLE! GET 30% OFF ALL NEW ARRIVALS.`,
+        bg: 'bg-white'
+    },
+    {
+        id: 3,
+        img: slider3,
+        title: 'NEW ARRIVALS',
+        desc: `DON'T COMPROMISE ON STYLE! GET 30% OFF ALL NEW ARRIVALS.`,
+        bg: 'bg-[#928DB5]'
+    }
+
+]
+
+
+
+
+
+
+
+
+
+export default sliderItems
